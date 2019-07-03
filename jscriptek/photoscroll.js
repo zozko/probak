7d9ad6e916a3035c1978ac67f121881aadddfb23
zozko <zozko@users.noreply.github.com>
2019-07-03 20:45:10 +0200

    a photoscrollhoz ujabb funkciok adva

--- jscriptek/photoscroll.js
+++ jscriptek/photoscroll.js
@@ -1,11 +1,23 @@
-let xk, yk;
-function mozog() {
-  yk = window.scrollY;
-  xk = window.scrollX;
-  // console.log("x koord = " + xk + "; y koord = " + yk);
-  document.getElementById(
-    "photo"
-  ).style.backgroundPosition = `${-xk}px ${-yk}px`;
-}
-// Events
-window.addEventListener("scroll", mozog);
+window.onload = function() {
+  let xk, yk;
+  let kep = document.getElementById("photo");
+
+  function mozog() {
+    yk = window.scrollY;
+    xk = window.scrollX;
+    console.log("x koord = " + xk + "; y koord = " + yk);
+    kep.style.backgroundPosition = `${-xk}px ${-yk}px`;
+  }
+
+  function kor() {
+    kep.style.borderRadius = "50%";
+  }
+
+  function nincskor() {
+    kep.style.borderRadius = "0%";
+  }
+  // Events;
+  window.addEventListener("scroll", mozog);
+  kep.addEventListener("mouseover", kor);
+  kep.addEventListener("mouseout", nincskor);
+};
