7dc378e2e3ac92d69f49014a7fe627a9efa76167
zozko <zozko.kis@gmail.com>
2020-06-06 10:47:15 +0200

    duplicated js kijavitva

--- duplicated.js
+++ duplicated.js
@@ -1,15 +1,13 @@
-let row = [2, 1, 3, 5, 3, 2];
+let nums = [2, 1, 3, 5, 5, 10, 3, 2];
 let count = 0;
 let firstDuplicite = -1;
-for (let i = 0; i < row.length - 1; i++) {
-    for (let j = 1; j <= row.length; j++) {
-        if (row.hasOwnProperty(row[j]) === row.hasOwnProperty(row[i])) {
-            count++;
-            if (count == 2) {
-                firstDuplicite = row[i];
-                count = 0;
-            }
+for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) {
+            firstDuplicite = nums[i];
+            console.log(firstDuplicite);
         }
+
     }
 }
 console.log(firstDuplicite);
