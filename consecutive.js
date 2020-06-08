d703dd7c3b77b0a87ad3a1eccc1478a42c627697
zozko <zozko.kis@gmail.com>
2020-06-08 19:42:56 +0200

    sorrendezo script - challenge

---
+++ consecutive.js
@@ -0,0 +1,21 @@
+/**
+ * ebben a kihivasban egy matrijoskat kell kipotolmi (ami a tomb elemei) a hianyzo elemekkel
+ *  
+ */
+
+function makeconsecutive(arr) {
+    arr.sort();
+    let neededItem = [];
+    let kezd = arr[0];
+    let vegez = arr[arr.length - 1];
+    for (kezd; kezd < vegez; kezd++) {
+        if (arr.indexOf(kezd) < 0) {
+            arr.unshift(kezd);
+            neededItem.push(kezd);
+        }
+    }
+    console.log(arr);
+    console.log(neededItem);
+}
+let arr = [6, 2, 3, 8];
+makeconsecutive(arr);
