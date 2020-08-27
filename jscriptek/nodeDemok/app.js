974f4ec101df15d3c96c1c051eeca72e30c2ee79
zozko <zozko.kis@gmail.com>
2020-08-27 23:02:53 +0200

    node folyt 27.8.2020

--- jscriptek/nodeDemok/app.js
+++ jscriptek/nodeDemok/app.js
@@ -1,3 +1,4 @@
+//ezek a modulok elerese - betoltese - a require() metodussal
 const http = require('http');
 const fsys = require('fs');
 
@@ -12,14 +13,14 @@ function reqListener(req, resp) {
 
     //hogy milyen metodussla komunikall a szerver: GET / POST
     const method = req.method;
-
+    let beirando = "";
     if (url === '/') {
         resp.write('<html>');
         resp.write('<head><title>input</title></head>');
         resp.write('<body><h1>ZOLO</h1>');
         resp.write('<form action="/message" method="POST"><input type="text" name="message" ></input><button type="submit">send</button></form>');
         resp.write('</html>');
-        //hogy ne fusson tovabb a valasz ezert returnal kell lezarni
+        //hogy ne fusson tovabb a valasz ezert returnal kell lezarni - mert ez egy callback funkcio
         return resp.end();
     }
     //kilepes a node szerverbol - hard exit a vegtelen hurokbol
@@ -39,21 +40,21 @@ function reqListener(req, resp) {
             const parosito = Buffer.concat(adatdarabokTeste).toString();
             console.log(parosito);
             //a tomb 2. reszet teszi bele a beirando valtozoba, ugy hogy az = szettori a stringet
-            const beirando = parosito.split('=')[1];
-            fsys.writeFileSync('message.txt', beirando);
+            beirando = parosito.split('=')[1];
+            fsys.writeFile('message.txt', beirando, err => {
+                resp.statuCode = 302;
+                resp.setHeader('Location', '/');
+                return resp.end();
+            });
+
         });
-
-        resp.statuCode = 302;
-        resp.setHeader('Location', '/');
-        return resp.end();
     }
-
-
     resp.setHeader('Content-Type', 'text/html');
     resp.write('<html>');
     resp.write('<head><title>zolo</title></head>');
-    resp.write('<body><h1>ZOLO</h1><p>megjott a valasz</p></body>');
-    resp.write('</html>');
+    resp.write('<body><h1>ZOLO</h1><p>megjott a valasz</p>');
+    resp.write('<p> csak nemlatom mi az.... viszont beirtam a message.txt fileba</p>');
+    resp.write('</body></html>');
     //a valasz veget jelezni kell az end()-al
     return resp.end();
 }
