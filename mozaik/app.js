c788f79b3eba82c4e894d0c6ba4546555995f12d
zozko <zozko.kis@gmail.com>
2022-04-21 23:37:54 +0200

    mozaik v1

--- mozaik/app.js
+++ mozaik/app.js
@@ -3,12 +3,15 @@ const dom = (function() {
     let container = document.querySelector('.photo_container');
 
 
+    let tileArr = [];
+
     let btn = document.querySelector('button');
 
     return {
         photoRam: container,
         bgPhto: photoArr,
-        gomb: btn
+        gomb: btn,
+        tileBlockArr: tileArr
     };
 
 })();
@@ -18,10 +21,30 @@ const picture = (function() {
 
     let showPics = dom.gomb.addEventListener('click', () => {
 
+        dom.tileBlockArr.length = 0;
+
+
+        if (dom.photoRam.hasChildNodes()) {
+            dom.photoRam.removeChild(dom.photoRam.childNodes[0]);
+        }
+
+        let tileWrapper = makeElem(dom.photoRam, 'div', 'tile_wrapper', 0);
         let picNum = Math.floor(Math.random() * 3);
-        let backgroundPicture = makeElem(dom.photoRam, 'img', 0, 0);
-        backgroundPicture.setAttribute('src', dom.bgPhto[picNum]);
-        backgroundPicture.setAttribute('alt', 'background photo');
+
+        dom.photoRam.style.background = `url(${dom.bgPhto[picNum]})`;
+        // let backgroundPicture = makeElem(dom.photoRam, 'img', 'kep', 0);
+
+        for (let k = 0; k < 50; k++) {
+            for (let i = 1; i <= 45; i++) {
+                let tile = makeElem(tileWrapper, 'div', 'tile', 0);
+                if (i % 45 === 1) {
+                    tile.classList.add('carry_left');
+                }
+                dom.tileBlockArr.push(tile);
+            }
+        }
+
+        tileOpacityRemover(dom.tileBlockArr);
     });
 
 
@@ -32,6 +55,45 @@ const picture = (function() {
 })();
 
 
+//az atlatszosagot levenni a takaro negyzetekrol - veletlenszeruen
+function tileOpacityRemover(array) {
+    console.log(array);
+    let tmbMeret = array.length;
+    console.log(tmbMeret);
+    let munkatomb = array;
+
+    let futtato = setInterval(() => {
+        munkatomb = setOpacity(munkatomb);
+        console.log('   tomb merete:', munkatomb.length);
+        if (munkatomb.length < 1) {
+            clearInterval(futtato);
+        }
+    }, 1);
+
+
+    // while (tmbMeret >= 0) {
+    //     setTimeout(() => {
+
+    //         array = setOpacity(munkatomb);
+    //         tmbMeret--;
+    //     }, 200);
+    // }
+}
+
+function setOpacity(aktualArr) {
+    let inportedArr = aktualArr.length;
+    let blockNumber = Math.floor(Math.random() * inportedArr);
+    console.log(`ezt torlom eppen ${blockNumber}`);
+
+    aktualArr[blockNumber].style.opacity = '0';
+    aktualArr.splice(blockNumber, 1);
+
+    return aktualArr;
+
+
+}
+
+
 
 
 //elem letrehozasa
@@ -39,10 +101,11 @@ function makeElem(parent, newTag, setClass, text) {
 
 
     //ha van a szulonek eleme, akkor toroljuk
-    if (parent.hasChildNodes()) {
-        parent.removeChild(parent.childNodes[0]);
-    }
+    // if (parent.hasChildNodes()) {
+    //     parent.removeChild(parent.childNodes[0]);
+    // }
     let elem = document.createElement(newTag);
+    if (setClass) elem.classList.add(setClass);
 
     parent.append(elem);
 
