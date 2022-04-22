eba497ab0f91dcb5ed9762e05141511187ffc110
zozko <zozko.kis@gmail.com>
2022-04-22 11:31:01 +0200

    irodaban keszitett munka

--- mozaik/app.js
+++ mozaik/app.js
@@ -1,114 +1,111 @@
-const dom = (function() {
-    let photoArr = ['photos/a.jpg', 'photos/b.jpg', 'photos/c.jpg'];
-    let container = document.querySelector('.photo_container');
+const dom = (function () {
+  let photoArr = ['photos/a.jpg', 'photos/b.jpg', 'photos/c.jpg'];
+  let container = document.querySelector('.photo_container');
 
+  let tileArr = [];
 
-    let tileArr = [];
+  let btn = document.querySelectorAll('button');
 
-    let btn = document.querySelector('button');
-
-    return {
-        photoRam: container,
-        bgPhto: photoArr,
-        gomb: btn,
-        tileBlockArr: tileArr
-    };
-
+  return {
+    photoRam: container,
+    bgPhto: photoArr,
+    gomb: btn,
+    tileBlockArr: tileArr,
+  };
 })();
 
+const picture = (function () {
+  let originArr;
+  let showPics = dom.gomb[0].addEventListener('click', () => {
+    dom.tileBlockArr.length = 0;
 
-const picture = (function() {
+    if (dom.photoRam.hasChildNodes()) {
+      dom.photoRam.removeChild(dom.photoRam.childNodes[0]);
+    }
 
-    let showPics = dom.gomb.addEventListener('click', () => {
+    let tileWrapper = makeElem(dom.photoRam, 'div', 'tile_wrapper', 0);
+    let picNum = Math.floor(Math.random() * 3);
 
-        dom.tileBlockArr.length = 0;
+    dom.photoRam.style.background = `url(${dom.bgPhto[picNum]})`;
+    // let backgroundPicture = makeElem(dom.photoRam, 'img', 'kep', 0);
 
-
-        if (dom.photoRam.hasChildNodes()) {
-            dom.photoRam.removeChild(dom.photoRam.childNodes[0]);
+    for (let k = 0; k < 50; k++) {
+      for (let i = 1; i <= 45; i++) {
+        let tile = makeElem(tileWrapper, 'div', 'tile', 0);
+        if (i % 45 === 1) {
+          tile.classList.add('carry_left');
         }
+        dom.tileBlockArr.push(tile);
+      }
+    }
+    originArr = dom.tileBlockArr.map((el) => el);
+    tileOpacity(dom.tileBlockArr, 'remove');
+  });
 
-        let tileWrapper = makeElem(dom.photoRam, 'div', 'tile_wrapper', 0);
-        let picNum = Math.floor(Math.random() * 3);
+  let fadePics = dom.gomb[1].addEventListener('click', () => {
+    // console.log('fading...?', originArr);
+    //  let blockNumber = Math.floor(Math.random() * originArr.length);
+    if (originArr != undefined && originArr.length > 0) {
+      tileOpacity(originArr, 'fade');
+    } else {
+      //   console.log('nothing to fade');
+    }
+  });
 
-        dom.photoRam.style.background = `url(${dom.bgPhto[picNum]})`;
-        // let backgroundPicture = makeElem(dom.photoRam, 'img', 'kep', 0);
-
-        for (let k = 0; k < 50; k++) {
-            for (let i = 1; i <= 45; i++) {
-                let tile = makeElem(tileWrapper, 'div', 'tile', 0);
-                if (i % 45 === 1) {
-                    tile.classList.add('carry_left');
-                }
-                dom.tileBlockArr.push(tile);
-            }
-        }
-
-        tileOpacityRemover(dom.tileBlockArr);
-    });
-
-
-
-    return {
-        bgPics: showPics
-    };
+  return {
+    bgPics: showPics,
+  };
 })();
-
 
 //az atlatszosagot levenni a takaro negyzetekrol - veletlenszeruen
-function tileOpacityRemover(array) {
-    console.log(array);
-    let tmbMeret = array.length;
-    console.log(tmbMeret);
-    let munkatomb = array;
+function tileOpacity(array, whatToDo) {
+  //   console.log(array);
+  let todo;
+  let originArr = array.map((el) => el);
+  let tmbMeret = array.length;
+  //   console.log(tmbMeret);
+  let munkatomb = array;
 
-    let futtato = setInterval(() => {
-        munkatomb = setOpacity(munkatomb);
-        console.log('   tomb merete:', munkatomb.length);
-        if (munkatomb.length < 1) {
-            clearInterval(futtato);
-        }
-    }, 1);
-
-
-    // while (tmbMeret >= 0) {
-    //     setTimeout(() => {
-
-    //         array = setOpacity(munkatomb);
-    //         tmbMeret--;
-    //     }, 200);
-    // }
+  if (whatToDo === 'remove') todo = 'rem';
+  if (whatToDo === 'fade') todo = 'cover';
+  let futtato = setInterval(() => {
+    munkatomb = setOpacity(munkatomb, todo);
+    // console.log('   tomb merete:', munkatomb.length);
+    if (munkatomb.length < 1) {
+      clearInterval(futtato);
+    }
+    // console.log('eredeti tomb: ', originArr);
+  }, 0);
 }
 
-function setOpacity(aktualArr) {
-    let inportedArr = aktualArr.length;
-    let blockNumber = Math.floor(Math.random() * inportedArr);
-    console.log(`ezt torlom eppen ${blockNumber}`);
+function setOpacity(aktualArr, makeIt) {
+  let inportedArr = aktualArr.length;
+  let blockNumber = Math.floor(Math.random() * inportedArr);
+  //   console.log(`ezt torlom eppen ${blockNumber}`);
 
+  if (makeIt === 'rem') {
     aktualArr[blockNumber].style.opacity = '0';
-    aktualArr.splice(blockNumber, 1);
+  }
 
-    return aktualArr;
+  if (makeIt === 'cover') {
+    aktualArr[blockNumber].style.opacity = '1';
+  }
 
+  aktualArr.splice(blockNumber, 1);
 
+  return aktualArr;
 }
-
-
-
 
 //elem letrehozasa
 function makeElem(parent, newTag, setClass, text) {
+  //ha van a szulonek eleme, akkor toroljuk
+  // if (parent.hasChildNodes()) {
+  //     parent.removeChild(parent.childNodes[0]);
+  // }
+  let elem = document.createElement(newTag);
+  if (setClass) elem.classList.add(setClass);
 
+  parent.append(elem);
 
-    //ha van a szulonek eleme, akkor toroljuk
-    // if (parent.hasChildNodes()) {
-    //     parent.removeChild(parent.childNodes[0]);
-    // }
-    let elem = document.createElement(newTag);
-    if (setClass) elem.classList.add(setClass);
-
-    parent.append(elem);
-
-    return elem;
-
-};
+  return elem;
+}
