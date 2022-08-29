589cdc1d0e5c6d42c1fd0ded130bcd9034ebf8a4
zozko <zozko.kis@gmail.com>
2022-08-29 21:39:49 +0200

    3 csillagig a kalkulacio kesz

--- profitipcalculator/logic.js
+++ profitipcalculator/logic.js
@@ -1,5 +1,5 @@
 const DOMelems = (function() {
-    const total = document.querySelector('.otal_value');
+    const total = document.querySelector('.total_value');
     const min = document.querySelector('.min_percent');
     const max = document.querySelector('.max_percent');
     const minNum = document.querySelector('.min_output');
@@ -10,12 +10,15 @@ const DOMelems = (function() {
     const downArrow = document.querySelector('.down_arrow');
     const rounder = document.querySelectorAll('[name="rounder"]');
     const saveBtn = document.querySelector('.save_data');
+    const makeCalculationBtn = document.querySelector('.let_Calc');
+    const displayTip = document.querySelector('.display_tip');
 
-    // console.log(saveBtn);
+    // console.log(displayTip);
 
 
     return {
         billTotal: total,
+        showCalc: displayTip,
         min_percent: min,
         max_percent: max,
         minOut: minNum,
@@ -25,18 +28,76 @@ const DOMelems = (function() {
         roundUP: upArrow,
         roundDOWN: downArrow,
         rounderInput: rounder,
-        setStorage: saveBtn
+        setStorage: saveBtn,
+        calc: makeCalculationBtn
     };
 })();
 
-console.log(DOMelems);
+// console.log(DOMelems);
 
 const manipulators = (function() {
-    let setRounder = 'DOWN';
+    let setRounder;
     let ratingStars;
     let minimumPercent = DOMelems.min_percent.value;
     let maximumPercent = DOMelems.max_percent.value;
+    let tip;
 
+
+    //calculations
+    DOMelems.calc.addEventListener('click', (event) => {
+        //TODO elso funkcio majd a localstorage ellenorzese
+
+        // a szamla osszegenek megszerzese
+        console.log(DOMelems.billTotal.value);
+        let billValue = +parseFloat(DOMelems.billTotal.value).toFixed(2);
+
+        //alap ratingStars beallitas OK
+        if (!ratingStars) {
+            DOMelems.ratingSetter.forEach(elem => {
+                // console.log(elem.getAttribute('checked'), elem.value);
+                if (elem.getAttribute('checked'))
+                    ratingStars = starSwitcher(parseInt(elem.value));
+            });
+        }
+        console.log('a csillag alapszama:', ratingStars);
+
+        //alap kerekites beallitasa - ellenorizni, hogy van e mentve a LocalStorageba
+        console.log('alap kerekites', setRounder);
+
+        // a minimum es maximum szazalek
+        console.log('min: ', minimumPercent, 'max: ', maximumPercent);
+
+
+
+
+
+        switch (ratingStars) {
+            case 1:
+                tip = oneStar(billValue);
+                break;
+            case 2:
+                tip = twoStars(billValue);
+                break;
+            case 3:
+                tip = threeStars(billValue);
+                break;
+
+            default:
+                console.log('lefutott');
+                tip = 0;
+                break;
+        }
+
+        //show tip
+        tip = +parseFloat(tip).toFixed(2);
+        // console.log(typeof billValue, typeof tip);
+        let fullBill = billValue + tip;
+        // console.log('ellenorzo', billValue, tip, fullBill);
+        DOMelems.showCalc.innerText = `at ${ratingStars} stars ratings our recomended tip is =${tip}.-!  Pay =${fullBill}.- `;
+
+
+
+    });
 
 
     let setMinPercent = DOMelems.min_percent.addEventListener('change', (ev) => {
@@ -73,70 +134,30 @@ const manipulators = (function() {
     DOMelems.minOut.innerText = setMinPercent || DOMelems.min_percent.value + '%';
     DOMelems.maxOut.innerText = setMaxPercent || DOMelems.max_percent.value + '%';
 
-    //rating star setting
-
+    //rating star setting - ezt nem mentjuk a localsorageba - minden eset individualis
     DOMelems.ratingSetter.forEach((elem) => {
         elem.addEventListener('change', (event) => {
             let starNumber = parseInt(elem.value);
             // console.log(elem, elem.value, starNumber);
-
-            switch (starNumber) {
-                case 1:
-                    console.log('cool1');
-                    ratingStars = 1;
-                    DOMelems.displayRating[0].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[1].src = 'imgs/star_OFF.png';
-                    DOMelems.displayRating[2].src = 'imgs/star_OFF.png';
-                    DOMelems.displayRating[3].src = 'imgs/star_OFF.png';
-                    DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
-
-                    break;
-                case 2:
-                    console.log('cool2');
-                    ratingStars = 2;
-                    DOMelems.displayRating[0].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[1].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[2].src = 'imgs/star_OFF.png';
-                    DOMelems.displayRating[3].src = 'imgs/star_OFF.png';
-                    DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
-                    break;
-                case 3:
-                    console.log('cool3');
-                    ratingStars = 3;
-                    DOMelems.displayRating[0].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[1].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[2].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[3].src = 'imgs/star_OFF.png';
-                    DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
-
-                    break;
-                case 4:
-                    console.log('cool4');
-                    ratingStars = 4;
-                    DOMelems.displayRating[0].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[1].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[2].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[3].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
-                    break;
-                case 5:
-                    console.log('cool5');
-                    ratingStars = 5;
-                    DOMelems.displayRating[0].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[1].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[2].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[3].src = 'imgs/star_ON.png';
-                    DOMelems.displayRating[4].src = 'imgs/star_ON.png';
-
-                    break;
-            }
-            console.log('csilla:', ratingStars);
+            ratingStars = starSwitcher(starNumber);
+            console.log('csillag:', ratingStars);
         });
     });
 
 
     // rounder methods
-    let rounder = DOMelems.rounderInput.forEach(elem => {
+    // let rounder =
+    DOMelems.rounderInput.forEach(elem => {
+        //default settings - ha lesz mentve localsorageba - ellenorizni, es az szerint allitani!
+        if (elem.getAttribute('checked') && elem.value === 'UP') {
+            setRounder = elem.value;
+            DOMelems.roundUP.style.border = '3px solid green';
+        }
+        if (elem.getAttribute('checked') && elem.value === 'DOWN') {
+            setRounder = elem.value;
+            DOMelems.roundDOWN.style.border = '3px solid green';
+        }
+
         elem.addEventListener('change', ev => {
             // console.log(elem.value);
             if (elem.value === 'UP') {
@@ -157,7 +178,7 @@ const manipulators = (function() {
     // save data to loclStorage
     DOMelems.setStorage.addEventListener('click', () => {
         // window.localStorage.clear();
-        window.localStorage.setItem('rating', ratingStars);
+        // window.localStorage.setItem('rating', ratingStars);
         window.localStorage.setItem('roundMethod', setRounder);
         window.localStorage.setItem('minPercent', minimumPercent);
         window.localStorage.setItem('maxPercent', maximumPercent);
@@ -167,3 +188,101 @@ const manipulators = (function() {
 
 
 })();
+
+
+//ez a funkcio allitja be a csillagokat az ertekuk alapjan
+function starSwitcher(numberOfStar) {
+    let rStar;
+    switch (numberOfStar) {
+        case 1:
+            // console.log('cool1');
+            rStar = 1;
+            DOMelems.displayRating[0].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[1].src = 'imgs/star_OFF.png';
+            DOMelems.displayRating[2].src = 'imgs/star_OFF.png';
+            DOMelems.displayRating[3].src = 'imgs/star_OFF.png';
+            DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
+
+            break;
+        case 2:
+            // console.log('cool2');
+            rStar = 2;
+            DOMelems.displayRating[0].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[1].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[2].src = 'imgs/star_OFF.png';
+            DOMelems.displayRating[3].src = 'imgs/star_OFF.png';
+            DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
+            break;
+        case 3:
+            // console.log('cool3');
+            rStar = 3;
+            DOMelems.displayRating[0].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[1].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[2].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[3].src = 'imgs/star_OFF.png';
+            DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
+
+            break;
+        case 4:
+            // console.log('cool4');
+            rStar = 4;
+            DOMelems.displayRating[0].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[1].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[2].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[3].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[4].src = 'imgs/star_OFF.png';
+            break;
+        case 5:
+            // console.log('cool5');
+            rStar = 5;
+            DOMelems.displayRating[0].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[1].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[2].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[3].src = 'imgs/star_ON.png';
+            DOMelems.displayRating[4].src = 'imgs/star_ON.png';
+
+            break;
+    }
+    return rStar;
+}
+
+
+//1 csillagos kalkulacio = visszaadja a szamlat - nincs jatt
+function oneStar(bill) {
+    return 0;
+}
+
+//2 csillagos kalkulacio = a szamlat felkerekiti a legkozelebbi egesz osszegre
+function twoStars(bill) {
+    let tipSum = +parseFloat(Math.ceil(bill)).toFixed(2);
+    return (tipSum - bill);
+}
+
+//3 csillagos kalkulaciov = kerekitjuk a szamlat a legkozelebbi 5 vagy 10 ertekre
+function threeStars(bill) {
+    // bill = 22,25
+
+    let tempBill = parseInt(bill);
+    let baseBill = tempBill;
+    let tempTip;
+    tempBill = tempBill % 10;
+    console.log('atmeneti szamla', tempBill);
+    if (tempBill < 5) {
+        tempTip = 5 - tempBill;
+        console.log('alap tipp: ', tempTip);
+    }
+
+    if (tempBill > 5) {
+        tempTip = 10 - tempBill;
+        console.log('alap tipp: ', tempTip);
+    }
+    let tizedesek = bill - baseBill;
+    tizedesek = tizedesek.toFixed(2);
+    console.log('tizedesek =', tizedesek);
+
+    return tempTip - tizedesek;
+}
+
+//4 csillagos kalkulacio
+
+//5 csillagos kalkulacio
