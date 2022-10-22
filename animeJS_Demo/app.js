0502bcd8128dd02f6d96393d83cc77bd34369547
zozko <zozko.kis@gmail.com>
2022-10-22 20:17:31 +0200

    new app

---
+++ animeJS_Demo/app.js
@@ -0,0 +1,12 @@
+anime({
+    targets: '.elem',
+    translateX: 300,
+    translateY: 20,
+    rotate: 90,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutQuad'
+})
+
+
+// anime();
