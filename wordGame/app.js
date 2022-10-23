ddec44a06353bd2a0e6e0820ff84729307ada72f
zozko <zozko.kis@gmail.com>
2022-10-23 12:49:19 +0200

    word game completed

--- wordGame/app.js
+++ wordGame/app.js
@@ -4,6 +4,7 @@ let controllers = document.querySelector('.controllers');
 let wordBox;
 let ind = 0;
 let shuffledArr = [];
+let shuffledWordsArr = [];
 let startTime;
 
 function oneWord(num) {
@@ -11,7 +12,7 @@ function oneWord(num) {
 
     .then(resp => resp.json())
         .then(data => {
-            console.log(data);
+            // console.log(data);
 
             wordBox = makeElemnt(playBox, 'div', 'wordBox');
             wordBox.innerText = data;
@@ -23,9 +24,14 @@ function oneWord(num) {
         }).catch(er => console.log('ERROR', er));
 }
 
-for (let i = 0; i < 6; i++) {
-    oneWord(i);
+function loadWords() {
+
+    for (let i = 0; i < 6; i++) {
+        oneWord(i);
+    }
 }
+
+loadWords();
 
 
 function startGame() {
@@ -37,6 +43,15 @@ function startGame() {
     shuffledArr = words.map(el => el);
     shuffledArr.sort((a, b) =>
         0.5 - Math.random());
+    shuffledArr.forEach(elem => {
+        tempWord = elem.split('');
+        // console.log(tempWord);
+        tempWord = tempWord.sort((a, b) =>
+            0.5 - Math.random());
+        tempWord = tempWord.join('');
+        // console.log('kevertbetukkel:', tempWord);
+        shuffledWordsArr.push(tempWord);
+    });
 
     let startBtn = makeElemnt(controllers, 'button', 'startBtn');
     startBtn.innerText = 'start game';
@@ -46,39 +61,49 @@ function startGame() {
 
 
 function game() {
-    console.log(startTime);
-    console.log(words, shuffledArr);
+    // console.log(startTime);
+    // console.log('alapTomb:', words, '  kevert tomb: ', shuffledArr, '  kevertbetuk tomb:', shuffledWordsArr);
     controllers.innerHTML = '';
 
     let searchFor = makeElemnt(controllers, 'p', 'searchFor');
-    searchFor.innerText = `talald meg : ${shuffledArr[ind]}`;
+    searchFor.innerText = `keresd meg : ${words[ind]}`;
     if (ind === 0) {
-        words.forEach(el => {
+        shuffledWordsArr.forEach((el, elindex) => {
             let gameBox = makeElemnt(playBox, 'div', 'gameBox');
             gameBox.innerHTML = el;
-            gameBox.addEventListener('click', test);
+            gameBox.addEventListener('click', (ev) => {
+                // console.log('elem indexe', elindex, '  words index:', ind);
+                if (shuffledArr[elindex] === words[ind]) {
+                    // console.log('OK');
+                    ind++
+                    ev.target.style.backgroundColor = '#f58c8c';
+                    game();
+                    if (ind >= words.length) {
+                        let endTime = new Date();
+                        controllers.innerHTML = 'GAME DURATION: ' + ((endTime - startTime) / 1000) + 's';
+                        // console.log('GAME OVER');
+                        playBox.innerHTML = '';
+                        let restartBtn = makeElemnt(controllers, 'button', 'restart');
+                        restartBtn.innerText = 'restart';
+                        restartBtn.addEventListener('click', () => {
+                            playBox.innerHTML = 'LOADING ...';
+                            controllers.innerHTML = '';
+                            ind = 0;
+                            words = [];
+                            shuffledArr = [];
+                            shuffledWordsArr = [];
+                            loadWords();
+                        });
+                    }
+                } else {
+                    // console.log('NEM OK...');
+                }
+            });
 
         });
         startTime = new Date();
     }
-}
-
-function test(ev) {
-    console.log(ev.target.innerHTML);
-    let clickedWord = ev.target.innerHTML;
-    if (clickedWord === shuffledArr[ind]) {
-        console.log('OK');
-        ind++
-        game();
-        if (ind >= words.length) {
-            let endTime = new Date();
-            controllers.innerHTML = 'GAME DURATION: ' + ((endTime - startTime) / 1000) + 's';
-            console.log('GAME OVER');
-        }
-    } else {
-        console.log('NEM OK...');
-    }
-}
+};
 
 
 function makeElemnt(parent, typ, className) {
